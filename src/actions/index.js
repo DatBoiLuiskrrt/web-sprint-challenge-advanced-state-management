6f0f74987664e7ca9de84839af2d9edd01a39dcf
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-01-21 11:44:23 -0600

    met mvp

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,52 @@
-import axios from 'axios';
+import axios from "axios";
 
+export const FETCH_START = "FETCH_START";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_FAIL = "FETCH_FAIL";
+export const ADD_SMURF = "ADD_SMURF";
+export const SET_ERROR = "SET_ERROR";
+
+export const fetchSmurfs = () => (dispatch) => {
+  dispatch(fetchStart());
+  axios
+    .get("http://localhost:3333/smurfs")
+    .then((res) => {
+      console.log(res.data);
+      dispatch(fetchSuccess(res.data));
+    })
+    .catch((err) => {
+      dispatch(fetchFail(err));
+    });
+};
+
+export const fetchStart = () => {
+  return { type: FETCH_START };
+};
+
+export const fetchSuccess = (smurf) => {
+  return { type: FETCH_SUCCESS, payload: smurf };
+};
+
+export const fetchFail = (error) => {
+  return { type: FETCH_FAIL, payload: error };
+};
+
+export const addSmurf = (smurf) => (dispatch) => {
+  dispatch(fetchStart());
+  axios
+    .post("http://localhost:3333/smurfs", smurf)
+    .then((res) => {
+      console.log(res.data);
+      dispatch(fetchSuccess(res.data));
+    })
+    .catch((err) => {
+      dispatch(fetchFail(err));
+    });
+};
+
+export const setError = (errorMessage) => {
+  return { type: SET_ERROR, payload: errorMessage };
+};
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,64 @@
+import {
+  FETCH_START,
+  FETCH_SUCCESS,
+  FETCH_FAIL,
+  ADD_SMURF,
+  SET_ERROR,
+} from "../actions";
 
 export const initialState = {
-}
+  smurfs: [],
+  isLoading: false,
+  error: "",
+};
 
-const reducer = ()=>{
-}
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_START:
+      return {
+        ...state,
+        smurfs: [],
+        isLoading: true,
+        error: "",
+      };
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        isLoading: false,
+        error: "",
+      };
+    case FETCH_FAIL:
+      return {
+        ...state,
+        smurfs: [],
+        isLoading: false,
+        error: action.payload,
+      };
+    case SET_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case ADD_SMURF:
+      const newSmurf = {
+        name: action.payload,
+        position: action.payload,
+        nickname: action.payload,
+        description: action.payload,
+        id: Date.now(),
+      };
+      return {
+        ...state,
+        smurfs: [...state.smurfs, newSmurf],
+        isLoading: false,
+        error: "",
+      };
+    default:
+      return state;
+  }
+};
 
-//**************DO NOT EDIT ANY CODE BEYOND THIS POINT**************//
 export default reducer;
 
 //Task List:
